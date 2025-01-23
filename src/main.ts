9a8b72312fa4134f39cb8a5b0e0691a37303129f
shoebilyas123 <shoebilyas123@gmail.com>
2025-01-23 23:03:46 +0530

    fix: code cleanup 0.0.1

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 import './style.css';
 
 // Cleaned up the code and will implement pseudo 3d again tomorrow
+// Currently the code doesn't include the backup folder that contains the actual code for pseudo-3d
